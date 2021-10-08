08588ecf76b9d2a49285e4407fa038f75743c784
andrebtw <gwandrewgd@gmail.com>
2021-10-08 20:45:38 +0200

    update

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+
+class Snake:
+    pass
+
+class ApplesSpawn:
+    pass
+
+
+
+
+
+
+def game():
+    game_end = False
+
+    while game_end == False:
+        pass
+
+
+def intro():
+    print("WELCOME TO THE SNAKE GAME !!!")
+    print("")
+    print("PRESS ENTER TO START THE GAME")
+    print("")
+    input()
+    game()
+
+
+def main():
+    intro()
+
+
+if __name__ == "__main__":
+    main()
