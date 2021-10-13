404916f012aec2fe3c4e5ca52ae5866310f2ab18
andrebtw <gwandrewgd@gmail.com>
2021-10-13 13:23:28 -0700

    Added "die" function and the snake can die when it reaches the walls.

--- main.py
+++ main.py
@@ -31,31 +31,68 @@ class Snake:
         self.color = blue_1
         self.dy = 300
         self.dx = 300
-
-    def die(self):
-        pass
+        self.speed = 3
+        self.score = 0
 
     def eat(self):
         pass
 
     def move(self, direction):
+        all_squares = []
+        for i in range(0, 600, 30):
+            all_squares.append(i)
+
         if direction == "up":
-            self.dy = self.dy + -2
+            self.dy = self.dy + - self.speed
             pygame.draw.rect(screen, self.color, (self.dx, self.dy, square, square))  # draw snake
         elif direction == "down":
-            self.dy = self.dy + 2
+            self.dy = self.dy + self.speed
             pygame.draw.rect(screen, self.color, (self.dx, self.dy, square, square))  # draw snake
         elif direction == "left":
-            self.dx = self.dx + -2
+            self.dx = self.dx + -self.speed
             pygame.draw.rect(screen, self.color, (self.dx, self.dy, square, square))  # draw snake
         elif direction == "right":
-            self.dx = self.dx + 2
+            self.dx = self.dx + self.speed
             pygame.draw.rect(screen, self.color, (self.dx, self.dy, square, square))  # draw snake
+
+    def coordinates(self):
+        x = self.dx
+        y = self.dy
+        return x, y
+
+
 
 
 class AppleSpawn:
     def __init__(self):
         pass
+
+
+def lose_text():
+    font = pygame.font.Font("cartoonist_kooky.ttf", 35)
+    lose_font = pygame.font.Font("cartoonist_kooky.ttf", 70)
+
+    lose_text = lose_font.render('YOU LOST !', True, white)
+    start_text = font.render('PRESS ANY KEY TO START THE GAME', True, white)
+
+    screen.blit(lose_text, (300 - lose_text.get_width() // 2, 200))  # Center the text
+    screen.blit(start_text, (300 - start_text.get_width() // 2, 385))  # Center the text
+
+
+def lose():
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYUP:
+                running = False
+                game()
+
+        background()
+        lose_text()
+        update()
 
 
 # Function to draw green squares all over the background
@@ -104,8 +141,6 @@ def enter_text():
     screen.blit(img, (300 - img.get_width() // 2, 285))  # Center the text
 
 
-
-
 def game():
     background()
     update()
@@ -135,6 +170,24 @@ def game():
                     print("right")
 
         background()
+
+        x, y = snake.coordinates()
+
+        #score = snake.score()
+
+        if x < 0:
+            running = False
+            lose()
+        if x > 600:
+            running = False
+            lose()
+        if y < 0:
+            running = False
+            lose()
+        if y > 600:
+            running = False
+            lose()
+
         snake.move(direction)
 
         update()
